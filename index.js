1474f207c1f244542d38ca132e064b558118bf9f
Jui Hsu <artistray@gmail.com>
2021-08-23 07:57:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+const eventName = `surprise`;
+
+function writeCards(names, eventName) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        debugger;
+    }
+
+    return (names);
+
+}
+
+writeCards(names);
+
+
+function countDown(number) {
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
